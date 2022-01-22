5ab22a30d982e6bbd063573e021dae56fb0b3500
mehedih23 <nmehedi23@gmail.com>
2022-01-22 11:44:32 +0600

    first commit

---
+++ contact-form.php
@@ -0,0 +1,25 @@
+<?php
+        $name = $_POST['name'];
+        $visitor_email = $_POST['email'];
+        $number = $_POST['number'];
+        $message = $_POST['message'];
+
+        $email_from = 'mehedin46@gmail.com';
+
+        $email_subject = "New Form Submission";
+
+        $email_body = "User Name : $name.\n". 
+                        "User Email : $visitor_email.\n". 
+                        "User Number : $number.\n". 
+                        "User Message : $message.\n";
+
+        $to = "nmehedi23@gmail.com";
+        $headers = "From : $email_from \r\n ";
+
+
+        $headers .= "Reply-To: $visitor_email \r\n;
+
+        mail($to,$email_subject, $email_body, $headers);
+
+        header("Location: index.html");
+?> 
